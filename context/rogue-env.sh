5f6d8c874ef86d7c503ba5f08fa62004d6ce9f1f
tomeichlersmith <eichl008@umn.edu>
2021-08-23 14:59:01 -0500

    copy more of Rogue's junk into the rogue-env
    hopefully this solves the linking error

--- context/rogue-env.sh
+++ context/rogue-env.sh
@@ -3,6 +3,18 @@
 ################################################################################
 # rogue-env.sh
 #   Set-up the Rogue environment inside the container
+#   A lot of this has been copied manually from Rogue's setup_rogue.sh
+#   script that is built when Rogue is compiled.
 ################################################################################
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}/usr/local/lib:${EPICS_BASE}/lib/linux-x86_64:${EPICS_PCAS_ROOT}/lib/linux-x86_64
+export ROGUE_DIR=/usr/local
+export ROGUE_VERSION=v5.9.3
+
+export PYTHONPATH=${PYTHONPATH:+:$PYTHONPATH}${ROGUE_DIR}/python
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}${ROGUE_DIR}/lib:${EPICS_BASE}/lib/linux-x86_64:${EPICS_PCAS_ROOT}/lib/linux-x86_64
+
+# PYDM environment
+export PYQTDESIGNERPATH=${ROGUE_DIR}/python/pyrogue/pydm:${PYQTDESIGNERPATH}
+export PYDM_DATA_PLUGINS_PATH=${ROGUE_DIR}/python/pyrogue/pydm/data_plugins
+export PYDM_TOOLS_PATH=${ROGUE_DIR}/python/pyrogue/pydm/tools
+
