032eff26ec4c084b83e8261b3b3377ec4e41db24
tomeichlersmith <eichl008@umn.edu>
2021-08-25 14:56:34 -0500

    able to compile the example custom rogue module

---
+++ rogue/MyModule/test/MyTest.py
@@ -0,0 +1,31 @@
+
+import pyrogue
+import MyModule
+
+class TestRoot(pyrogue.Root):
+    def __init__(self):
+        super().__init__(name="MyRoot")
+
+        # Add master and slave devices
+        self.add(MyModule.MyCustomMaster(name="testMaster"))
+        self.add(MyModule.MyCustomSlave(name="testSlave"))
+
+        # Connect master to slave
+        self.testMaster >> self.testSlave
+
+        # Start the tree
+        self.start()
+
+
+with TestRoot() as r:
+    # Set frame size to 200
+    r.testMaster.FrameSize.set(200)
+
+    # Generate 2 Frames
+    r.testMaster.MyFrameGen()
+    r.testMaster.MyFrameGen()
+
+    # Display status
+    print("Sent {} bytes in {} frames".format(r.testMaster.ByteCount.get(),r.testMaster.FrameCount.get()))
+    print("Got  {} bytes in {} frames".format(r.testSlave.ByteCount.get(),r.testSlave.FrameCount.get()))
+
--- rogue/README.md
+++ rogue/README.md
@@ -9,3 +9,9 @@ I would like to emphasize two points.
 2. The front-end firmware and hardware is also in development.
 
 Both of these points mean that this repository is most certainly **not** stable.
+
+## Table of Contents
+
+### MyModule
+[Example custom module from Rogue](https://slaclab.github.io/rogue/custom_module/index.html)
+with some tweaks to get it to compile within our environment image.
